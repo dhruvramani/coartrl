30c026ce5a01b06d5197906045c9246059e0a7eb
Dhruv <dhruvramani98@gmail.com>
2019-04-25 19:22:00 +0530

    dummmmyyy

--- src/dummy.py
+++ src/dummy.py
@@ -7,10 +7,10 @@ from stable_baselines.ddpg.policies import MlpPolicy
 
 env = gym.make('FetchReach-v1')
 model = HER(MlpPolicy, env=env, model_class=DDPG)
-model.learn(total_timesteps=5000)
+model.learn(total_timesteps=50000)
 
 obs = env.reset()
-for i in range(100):
+for i in range(10000):
     action, _states = model.predict(obs)
     obs, rewards, done, _ = env.step(action)
     env.render()
