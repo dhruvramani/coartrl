00dc1cbef188e6f9f396d3596f3f0cf4eb5e325c
Dhruv <dhruvramani98@gmail.com>
2019-04-26 06:41:39 +0530

    dummy check Q value

--- src/dummy.py
+++ src/dummy.py
@@ -4,11 +4,9 @@ import numpy as np
 from her.her import HER
 from ddpg.ddpg import DDPG
 from stable_baselines.ddpg.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
 
 
 env = gym.make('FetchReach-v1')
-env = DummyVecEnv([lambda: env])
 model = HER(MlpPolicy, env=env, model_class=DDPG)
 model.learn(total_timesteps=100)
 
@@ -20,5 +18,7 @@ model = DDPG.load("../policies/dummy.pol")
 obs = env.reset()
 for i in range(10):
     action, _states = model.predict(obs)
+    qvalue = model.model.value(obs)
+    print(action, qvalue)
     obs, rewards, done, _ = env.step(action)
     env.render()
