08f5d3b31817bcf0a2d7a1b8c072b1686c48266e
Dhruv <dhruvramani98@gmail.com>
2019-06-12 23:09:59 +0530

    Alpha = 1

--- transition/main.py
+++ transition/main.py
@@ -72,7 +72,7 @@ def coarticulation(env, primitive_pi, config):
 
     trainer = RLTrainer(env, coart_pi, coart_oldpi, primitive_pi, config)
     # NOTE : Will change the meaning of alpha later
-    rollout = rollouts.traj_segment_generator_coart(env, primitive_pi, coart_pi, alpha=0.5, stochastic=not config.is_collect_state, config=config)
+    rollout = rollouts.traj_segment_generator_coart(env, primitive_pi, coart_pi, alpha=1.0, stochastic=not config.is_collect_state, config=config)
 
     print("Training Co-Articulations")
     trainer.train(rollout)
