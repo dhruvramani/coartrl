934b8bcef6c61d5ddfee332ff1c8417cb9d18ed9
Dhruv <dhruvramani98@gmail.com>
2019-06-17 10:48:54 +0530

    trying without tanh

--- transition/sac/core.py
+++ transition/sac/core.py
@@ -86,7 +86,7 @@ def mlp_actor_critic(x, a, hidden_sizes=(400,300), activation=tf.nn.relu,
     # policy
     with tf.variable_scope('pi'):
         mu, pi, logp_pi = policy(x, a, hidden_sizes, activation, output_activation)
-        mu, pi, logp_pi = apply_squashing_func(mu, pi, logp_pi)
+        #mu, pi, logp_pi = apply_squashing_func(mu, pi, logp_pi)
 
     # make sure actions are in correct range
     action_scale = action_space.high[0]
