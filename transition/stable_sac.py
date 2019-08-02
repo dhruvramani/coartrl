3cf342b438ffd4c6d0f82283ee1ade1df644ffb3
Dhruv <dhruvramani98@gmail.com>
2019-08-02 20:57:19 +0530

    Added Stable SAC

---
+++ transition/stable_sac.py
@@ -0,0 +1,14 @@
+import gym
+import numpy as np
+
+from stable_baselines.sac.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import SAC
+
+#env = gym.make('Pendulum-v0')
+
+def stable_sac(env):
+	env = DummyVecEnv([lambda: env])
+	model = SAC(MlpPolicy, env, verbose=1)
+	model.learn(total_timesteps=50000, log_interval=10)
+	model.save("sac_pendulum")
